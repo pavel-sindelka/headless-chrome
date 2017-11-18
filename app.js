62d76f09782174b5070a81840809ce1f58096a6d
Pavel Šindelka <sindelka95@gmail.com>
2017-11-18 20:04:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/', function(req, res) {
         await page.click('input[type="submit"]');
         await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
-        await page.evaluate("body.innerHTML = '';");
+        await page.evaluate("document.body.innerHTML = '';");
         //const matchs = await page.$("#ss16");
         //console.log("------------------");
         //console.log(matchs);
