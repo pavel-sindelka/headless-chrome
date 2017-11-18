41fc2584a1c376fd50b1e931cbf0b7b528d9b1d1
Pavel Šindelka <sindelka95@gmail.com>
2017-11-18 19:10:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.get('/', function(req, res) {
         await page.type('#userNameId', 'sindelka95');
         await page.type('#passwordId', 'sindelka');
         await page.click('input[type="submit"]');
-        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        await page.waitForNavigation({ waitUntil: 'load' });
 
-        //await page.evaluate("var matchs = document.getElementById('ss16').getElementsByClassName('match'); var index; for (var i = 0; i < matchs.length; i++) { var teams = matchs[i].getElementsByClassName('nameMatch')[0].innerHTML.split(' - '); var text = teams[0]; var tip = 'a'; var reg = '.*' + tip.replace(/\\s/g,'').split('').join('+.*') + '+.*'; var match = text.match(new RegExp(reg, 'i')); console.log(match); if (match !== null) { index = i; break; } } console.log(index); matchs[index].click();")
+        await page.evaluate("var matchs = document.getElementById('ss16').getElementsByClassName('match'); var index; for (var i = 0; i < matchs.length; i++) { var teams = matchs[i].getElementsByClassName('nameMatch')[0].innerHTML.split(' - '); var text = teams[0]; var tip = 'a'; var reg = '.*' + tip.replace(/\\s/g,'').split('').join('+.*') + '+.*'; var match = text.match(new RegExp(reg, 'i')); console.log(match); if (match !== null) { index = i; break; } } console.log(index); matchs[index].click();")
         //await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
         
         await page.screenshot({ fullPage: true }).then(function(buffer) {
