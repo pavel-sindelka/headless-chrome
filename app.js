8f67e57f6b3407947585dbd6406d6805c9df71ac
Pavel Šindelka <sindelka95@gmail.com>
2017-11-18 21:37:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.get('/', function(req, res) {
         await page.type('#userNameId', 'sindelka95');
         await page.type('#passwordId', 'sindelka');
         await page.click('input[type="submit"]');
-        await page.waitForNavigation({ waitUntil: 'load' });
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
-        await page.evaluate("var matchs = document.getElementById('ss16'); document.body[0].innerHTML = matchs;");
+        await page.evaluate("var matchs = document.getElementById('ss16'); document.getElementsByTagName('body')[0].innerHTML = matchs;");
         //const matchs = await page.$("#ss16");
         //console.log("------------------");
         //console.log(matchs);
