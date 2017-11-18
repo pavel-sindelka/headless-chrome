f76f4cb4f928e73f67deb4d3f664b8ed0a73318f
Pavel Šindelka <sindelka95@gmail.com>
2017-11-18 19:00:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,36 @@
 const express = require('express');
-const app = express();
 const puppeteer = require('puppeteer');
+
+const app = express();
 const port = process.env.PORT || 8080;
-const validUrl = require('valid-url');
-
-var parseUrl = function(url) {
-    url = decodeURIComponent(url)
-    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
-        url = 'http://' + url;
-    }
-
-    return url;
-};
+const url = "https://www.tipsport.cz/live";
 
 app.get('/', function(req, res) {
-    var urlToScreenshot = "https://tipsport.cz";
+    (async() => {
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
 
-    if (validUrl.isWebUri(urlToScreenshot)) {
-        console.log('Screenshotting: ' + urlToScreenshot);
-        (async() => {
-            const browser = await puppeteer.launch({
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
-            });
+        const page = await browser.newPage();
 
-            const page = await browser.newPage();
-            
-            await page.goto('https://www.tipsport.cz/live', { waitUntil: 'domcontentloaded' });
-            
-            await page.type('#userNameId', 'sindelka95');
-            await page.type('#passwordId', 'sindelka');
-            await page.click('input[type="submit"]');
-            await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-            
+        await page.goto('https://www.tipsport.cz/live', { waitUntil: 'domcontentloaded' });
 
-            
-            await page.screenshot({ fullPage: true }).then(function(buffer) {
-                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
-                res.setHeader('Content-Type', 'image/png');
-                res.send(buffer)
-            });
+        await page.type('#userNameId', 'sindelka95');
+        await page.type('#passwordId', 'sindelka');
+        await page.click('input[type="submit"]');
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
-            await browser.close();
-        })();
-    } else {
-        res.send('Invalid url: ' + urlToScreenshot);
-    }
+        await page.evaluate("var matchs = document.getElementById('ss16').getElementsByClassName('match'); var index; for (var i = 0; i < matchs.length; i++) { var teams = matchs[i].getElementsByClassName('nameMatch')[0].innerHTML.split(' - '); var text = teams[0]; var tip = 'a'; var reg = '.*' + tip.replace(/\\s/g,'').split('').join('+.*') + '+.*'; var match = text.match(new RegExp(reg, 'i')); console.log(match); if (match !== null) { index = i; break; } } console.log(index); matchs[index].click();")
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        
+        await page.screenshot({ fullPage: true }).then(function(buffer) {
+            res.setHeader('Content-Disposition', 'attachment;filename="' + url + '.png"');
+            res.setHeader('Content-Type', 'image/png');
+            res.send(buffer);
+        });
 
+        await browser.close();
+    })();
 });
 
 app.listen(port, function() {
