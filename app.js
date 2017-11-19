2083feb34e590b69610d82e5e62c104683d1cdd6
Pavel Šindelka <sindelka95@gmail.com>
2017-11-19 21:35:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.get('/', function(req, res) {
 
         //await page.evaluate("var matchs = document.getElementById('ss16').getElementsByClassName('match'); var index; for (var i = 0; i < matchs.length; i++) { var teams = matchs[i].getElementsByClassName('nameMatch')[0].innerHTML.split(' - '); var text = teams[0]; var tip = 'a'; var reg = '.*' + tip.replace(/\\s/g,'').split('').join('+.*') + '+.*'; var match = text.match(new RegExp(reg, 'i')); console.log(match); if (match !== null) { index = i; break; } } console.log(index); matchs[index].click();");
         */
+        
+        await page.evaluate(() => document.body.innerHTML = "";
         await page.screenshot({ fullPage: true }).then(function(buffer) {
             res.setHeader('Content-Disposition', 'attachment;filename="' + url + '.png"');
             res.setHeader('Content-Type', 'image/png');
