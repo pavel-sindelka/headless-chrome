2fbdba3523556b325950288b7de2d2895900ba3f
Pavel Šindelka <sindelka95@gmail.com>
2017-11-23 04:14:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+var cluster = require('cluster');
+
+if(cluster.isMaster) {
+    var numWorkers = require('os').cpus().length;
+
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+    for(var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
 const express = require('express');
 const puppeteer = require('puppeteer');
 const numCPUs = require('os').cpus().length;
@@ -73,3 +94,4 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
     console.log('App listening on port ' + port)
 })
+} 
