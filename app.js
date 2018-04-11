eff630df13169893a2d2586cd07ba9fa97e63150
Pavel Šindelka <sindelka95@gmail.com>
2018-04-11 16:48:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-var cluster = require('cluster');
-var numWorkers = require('os').cpus().length;
-
 const express = require('express');
 const puppeteer = require('puppeteer');
-const numCPUs = require('os').cpus().length;
+
 const app = express();
 const port = process.env.PORT || 8080;
 const url = "https://www.tipsport.cz/live";
 
 const newPage = async (res) => {
-        console.log(numWorkers);
         const browser = await puppeteer.launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
-console.log("a ahaaa a");
+
         const page = await browser.newPage();
-console.log("wwwwww");
+
         await page.goto('https://www.tipsport.cz/live');
-console.log("mmmm mm");
+
         await page.type('#userNameId', 'sindelka95');
         await page.type('#passwordId', 'sindelka');
         await page.click('input[type="submit"]');
@@ -59,48 +55,19 @@ console.log("mmmm mm");
         
         //await page.waitFor(5000);
         
-        const buffer = await page.screenshot({ fullPage: true });
-    
+        await page.screenshot({ fullPage: true }).then(function(buffer) {
+            res.setHeader('Content-Disposition', 'attachment;filename="' + url + '.png"');
+            res.setHeader('Content-Type', 'image/png');
+            res.send(buffer);
+        });
+
         await browser.close();
-    console.log("eeeeeeee");
-    return buffer;
 }
 
-if(cluster.isMaster) {
-    console.log('Master cluster setting up ' + numWorkers + ' workers...');
-
-    for(var i = 0; i < 1; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('online', function(worker) {
-        console.log('Worker ' + worker.process.pid + ' is online');
-    });
-
-    cluster.on('exit', function(worker, code, signal) {
-        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
-        console.log('Starting a new worker');
-        cluster.fork();
-    });
-    
-    newPage();
-    newPage();
-} else {
-
-
-
-
-app.get('/', async function(req, res) {
-        newPage(res);
-        newPage(res);
-
-     res.setHeader('Content-Disposition', 'attachment;filename="' + url + '.png"');
-            res.setHeader('Content-Type', 'image/png');
-            res.send();
+app.get('/', function(req, res) {
+    newPage(res);
 });
 
 app.listen(port, function() {
     console.log('App listening on port ' + port)
-    console.log('Process ' + process.pid + ' is listening to all incoming requests');
 })
-} 
